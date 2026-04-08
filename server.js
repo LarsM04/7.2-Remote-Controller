9809a3627a2b1b9efbdc4fe7174635e34acde1ad
LarsM04 <larsmudde0810@gmail.com>
2026-04-08 10:44:08 +0200

    Fix voor Render deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const path = require('path');
 const WebSocket = require('ws');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // MIME types for static files
 const mimeTypes = {
